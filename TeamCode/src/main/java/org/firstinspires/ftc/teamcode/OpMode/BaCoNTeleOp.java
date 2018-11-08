d3204c63c0dee6cd321e83d9c9fabd565a8ae6a6
Haydn Trigg <me@haydntrigg.com>
2018-11-08 17:21:17 +1030

    New Drive class and new BaCoNTeleOp class (main teleop class)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/BaCoNTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/BaCoNTeleOp.java
@@ -43,55 +43,18 @@ import com.qualcomm.robotcore.util.Range;
  * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
  * class is instantiated on the Robot Controller and executed.
  *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * This particular OpMode just executes a basic Tank BaCoNTeleOp Teleop for a two wheeled robot
  * It includes all the skeletal structure that all linear OpModes contain.
  *
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Basic: Linear OpMode", group="Linear Opmode")
-
-public class Drive extends LinearOpMode {
+@TeleOp
+public class BaCoNTeleOp extends LinearOpMode {
 
     // Declare OpMode members.
-    private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor front_left_motor = null;
-    private DcMotor front_right_motor = null;
-    private DcMotor rear_left_motor = null;
-    private DcMotor rear_right_motor = null;
-
-    private void updateDrive(){
-        // Setup a variable for each drive wheel to save power level for telemetry
-        double front_left_power;
-        double front_right_power;
-        double rear_left_power;
-        double rear_right_power;
-
-        // Implement Mecanum drive using drive equations from Internet:
-        double left_y = gamepad1.left_stick_y;
-        double left_x  =  gamepad1.left_stick_x;
-        double right_x = gamepad1.right_stick_x;
-        front_left_power = Range.clip(left_y + left_x + right_x, -1.0, 1.0);
-        rear_left_power = Range.clip(left_y + left_x - right_x, -1.0, 1.0);
-        front_right_power = Range.clip(left_y - left_x - right_x, -1.0, 1.0);
-        rear_right_power = Range.clip(left_y - left_x + right_x, -1.0, 1.0);
-
-        // Send calculated power to wheels
-        front_left_motor.setPower(front_left_power);
-        front_right_motor.setPower(front_right_power);
-        rear_left_motor.setPower(rear_left_power);
-        rear_right_motor.setPower(rear_right_power);
-
-        // Show the elapsed game time and wheel power.
-        telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("Motors", "front left (%.2f), front right (%.2f)",
-                front_left_power, front_right_power);
-        telemetry.addData("Motors", "rear left (%.2f), rear right (%.2f)",
-                rear_left_power, rear_right_power);
-        telemetry.update();
-
-    }
+    public ElapsedTime runtime = new ElapsedTime();
 
     private void updateLift(){
 
@@ -102,20 +65,7 @@ public class Drive extends LinearOpMode {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
-        front_left_motor  = hardwareMap.get(DcMotor.class, "front_left_drive");
-        front_right_motor = hardwareMap.get(DcMotor.class, "front_right_drive");
-        rear_left_motor  = hardwareMap.get(DcMotor.class, "rear_left_drive");
-        rear_right_motor = hardwareMap.get(DcMotor.class, "rear_right_drive");
-
-        // Most robots need the motor on one side to be reversed to drive forward
-        // Reverse the motor that runs backwards when connected directly to the battery
-        front_left_motor.setDirection(DcMotor.Direction.FORWARD);
-        front_right_motor.setDirection(DcMotor.Direction.FORWARD);
-        rear_left_motor.setDirection(DcMotor.Direction.FORWARD);
-        rear_right_motor.setDirection(DcMotor.Direction.FORWARD);
+        Drive drive = new Drive(this);
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -124,8 +74,10 @@ public class Drive extends LinearOpMode {
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
-            updateDrive();
+            drive.updateDrive();
             updateLift();
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
 
         }
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmode/Drive.java
@@ -1,66 +1,40 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode.OpMode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-
 /**
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all linear OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ * Created by Baconeers on 8/11/2018.
  */
 
-@TeleOp(name="Basic: Linear OpMode", group="Linear Opmode")
-
-public class Drive extends LinearOpMode {
-
-    // Declare OpMode members.
-    private ElapsedTime runtime = new ElapsedTime();
+public class Drive {
+    // members:
+    private OpMode opmode = null;
     private DcMotor front_left_motor = null;
     private DcMotor front_right_motor = null;
     private DcMotor rear_left_motor = null;
     private DcMotor rear_right_motor = null;
 
+    public Drive(OpMode opmodeIn) {
+        super();
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        opmode = opmodeIn;
+        front_left_motor  = opmode.hardwareMap.get(DcMotor.class, "front_left_drive");
+        front_right_motor = opmode.hardwareMap.get(DcMotor.class, "front_right_drive");
+        rear_left_motor  = opmode.hardwareMap.get(DcMotor.class, "rear_left_drive");
+        rear_right_motor = opmode.hardwareMap.get(DcMotor.class, "rear_right_drive");
+
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        front_left_motor.setDirection(DcMotor.Direction.FORWARD);
+        front_right_motor.setDirection(DcMotor.Direction.FORWARD);
+        rear_left_motor.setDirection(DcMotor.Direction.FORWARD);
+        rear_right_motor.setDirection(DcMotor.Direction.FORWARD);
+
+    }
     private void updateDrive(){
         // Setup a variable for each drive wheel to save power level for telemetry
         double front_left_power;
@@ -69,9 +43,9 @@ public class Drive extends LinearOpMode {
         double rear_right_power;
 
         // Implement Mecanum drive using drive equations from Internet:
-        double left_y = gamepad1.left_stick_y;
-        double left_x  =  gamepad1.left_stick_x;
-        double right_x = gamepad1.right_stick_x;
+        double left_y = opmode.gamepad1.left_stick_y;
+        double left_x  =  opmode.gamepad1.left_stick_x;
+        double right_x = opmode.gamepad1.right_stick_x;
         front_left_power = Range.clip(left_y + left_x + right_x, -1.0, 1.0);
         rear_left_power = Range.clip(left_y + left_x - right_x, -1.0, 1.0);
         front_right_power = Range.clip(left_y - left_x - right_x, -1.0, 1.0);
@@ -84,49 +58,11 @@ public class Drive extends LinearOpMode {
         rear_right_motor.setPower(rear_right_power);
 
         // Show the elapsed game time and wheel power.
-        telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("Motors", "front left (%.2f), front right (%.2f)",
+        opmode.telemetry.addData("Motors", "front left (%.2f), front right (%.2f)",
                 front_left_power, front_right_power);
-        telemetry.addData("Motors", "rear left (%.2f), rear right (%.2f)",
+        opmode.telemetry.addData("Motors", "rear left (%.2f), rear right (%.2f)",
                 rear_left_power, rear_right_power);
-        telemetry.update();
+        opmode.telemetry.update();
 
-    }
-
-    private void updateLift(){
-
-    }
-
-    @Override
-    public void runOpMode() {
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
-
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
-        front_left_motor  = hardwareMap.get(DcMotor.class, "front_left_drive");
-        front_right_motor = hardwareMap.get(DcMotor.class, "front_right_drive");
-        rear_left_motor  = hardwareMap.get(DcMotor.class, "rear_left_drive");
-        rear_right_motor = hardwareMap.get(DcMotor.class, "rear_right_drive");
-
-        // Most robots need the motor on one side to be reversed to drive forward
-        // Reverse the motor that runs backwards when connected directly to the battery
-        front_left_motor.setDirection(DcMotor.Direction.FORWARD);
-        front_right_motor.setDirection(DcMotor.Direction.FORWARD);
-        rear_left_motor.setDirection(DcMotor.Direction.FORWARD);
-        rear_right_motor.setDirection(DcMotor.Direction.FORWARD);
-
-        // Wait for the game to start (driver presses PLAY)
-        waitForStart();
-        runtime.reset();
-
-        // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
-
-            updateDrive();
-            updateLift();
-
-        }
     }
 }
